79418a004e0a289f4fbe7a7ba0e564789b4185bc
Cristian Giraldo <cgiraldo@unal.edu.co>
2024-12-08 19:16:45 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https//cristiangivi.github.io/prueba-frontend-react'
+  base: 'Prueba-frontend-React'
 })
